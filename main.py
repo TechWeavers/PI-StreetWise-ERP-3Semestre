4f582da1d234ff5a1bbdd259799502f80c1f2e71
joao pedro <jplima.dev@outlook.com>
2024-04-13 14:40:13 -0300

    model de cliente

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-from fastapi import FastAPI
-app = FastAPI()
+
+import datetime
+def obterDataAtual():
+  data_atual = datetime.datetime.now()
+  dia = data_atual.strftime("%d")
+  mes = data_atual.strftime("%m")
+  ano = data_atual.strftime("%Y")
+  data_format = dia+"/"+mes+"/"+ano
+  return data_format
+obterDataAtual()
