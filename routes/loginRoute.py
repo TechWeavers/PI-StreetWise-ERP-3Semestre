8696a817221bb4a681ae0e5b692e2008f86610f7
joao pedro <jplima.dev@outlook.com>
2024-04-22 01:07:44 -0300

    bug_auth

--- routes/loginRoute.py
+++ routes/loginRoute.py
@@ -50,12 +50,3 @@ async def read_users_me(
 @app.get("/produto")
 async def obterProduto(token: str = Depends(oauth2_scheme)):
     return {"produto": {"nome": "mouse", "preço": 200}}
-
-app.include_router(userAPI)
-
-if __name__ == "__loginRoute__":
-    import uvicorn
-    try:
-        uvicorn.run(app, host="0.0.0.0", port=8000)
-    except (KeyboardInterrupt, asyncio.CancelledError):
-        pass
