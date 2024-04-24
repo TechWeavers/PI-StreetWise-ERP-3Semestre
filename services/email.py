1d94ed9e89fc327eea6400f022e14a414a663f08
joao pedro <jplima.dev@outlook.com>
2024-04-23 23:48:52 -0300

    servico email ainda nao concluido

---
+++ services/email.py
@@ -0,0 +1,44 @@
+from typing import List
+from fastapi import BackgroundTasks, FastAPI
+from fastapi_mail import ConnectionConfig, FastMail, MessageSchema, MessageType
+from pydantic import BaseModel, EmailStr
+from starlette.responses import JSONResponse
+
+
+
+class EmailSchema(BaseModel):
+    email: List[EmailStr]
+
+
+conf = ConnectionConfig(
+    MAIL_USERNAME ="sixdevsfatec@gmail.com",
+    MAIL_PASSWORD = "9243 2979",
+    MAIL_FROM = "sixdevsfatec@gmail.com",
+    MAIL_PORT = 465,
+    MAIL_SERVER = "smtp.gmail.com",
+    MAIL_STARTTLS = False,
+    MAIL_SSL_TLS = True,
+    USE_CREDENTIALS = True,
+    VALIDATE_CERTS = True
+)
+
+app = FastAPI()
+
+
+html = """
+<p>Thanks for using Fastapi-mail</p> 
+"""
+
+
+@app.post("/email")
+async def simple_send(email: EmailSchema) -> JSONResponse:
+
+    message = MessageSchema(
+        subject="Fastapi-Mail module",
+        recipients= dict(email).get("email"),
+        body=html,
+        subtype=MessageType.html)
+
+    fm = FastMail(conf)
+    await fm.send_message(message)
+    return JSONResponse(status_code=200, content={"message": "email has been sent"})  
