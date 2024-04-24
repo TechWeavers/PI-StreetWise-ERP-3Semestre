df4c54f9d5f926a9c099ce53a1029d1e893ea132
joaom <jplima.dev@outlook.com>
2024-04-24 11:26:40 -0300

    login_refat

---
+++ Controllers/token.py
@@ -0,0 +1,27 @@
+from fastapi import HTTPException, status, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import jwt
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+
+class Token:
+    def __init__(self):
+        self.SECRET_KEY = "FPaDbtjzU9r9kziJMkkkprJ8cVcEun6QyPf8XfSRdi2DJ56a6Wwhd32u9e8hdub" #assinatura do token
+        self.ALGORITHM = 'HS256'
+
+    def verificar_token(self, token: str) -> dict:
+        try:
+            payload = jwt.decode(token, self.SECRET_KEY, algorithms=[self.ALGORITHM])
+            return payload
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expirado")
+        except jwt.InvalidTokenError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token inválido")
+
+    def gerar_token(self, id: str) -> str:
+        payload = {
+            'sub': id,
+            'exp': datetime.now(datetime) + timedelta(days=1) 
+        }
+        jwt_token = jwt.encode(payload, self.SECRET_KEY, algorithm=self.ALGORITHM)
+        return jwt_token
