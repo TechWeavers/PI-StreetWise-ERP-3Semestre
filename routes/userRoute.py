483f334dda45c92a2d133b636cd4cdb37444dd59
joao pedro <jplima.dev@outlook.com>
2024-05-02 02:05:51 -0300

    bug_atualizar

--- routes/userRoute.py
+++ routes/userRoute.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], 
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
     allow_headers=["*"],
 )
 
