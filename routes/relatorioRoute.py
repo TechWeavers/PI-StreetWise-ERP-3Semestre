bd22dfe196a57699a4dafb714ef65083b4256fd2
joao pedro <jplima.dev@outlook.com>
2024-05-30 21:07:29 -0300

    realtorios com auth

--- routes/relatorioRoute.py
+++ routes/relatorioRoute.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, FastAPI, Depends,Header
 from routes.loginRoute import validar_token
 from typing import Annotated
 from fastapi.middleware.cors import CORSMiddleware
-from models.clienteModel import Cliente
 from Controllers.Controller_Material import ControllerMaterial
 from Controllers.Controller_Agenda import Controller_Copia_Agendamento
 
@@ -18,25 +17,25 @@ app.add_middleware(
 )
 
 @relatorioAPI.get("/materiais-faltantes", tags=["relatorios"])
-async def buscar_materiais_faltantes():
+async def buscar_materiais_faltantes(Authorization: Annotated[Header, Depends(validar_token)]):
   return ControllerMaterial.CalcularMateriaisFaltantes()
 
 @relatorioAPI.get("/proximos-agendamentos", tags=["relatorios"])
-async def buscar_proximos_agendamentos():
+async def buscar_proximos_agendamentos(Authorization: Annotated[Header, Depends(validar_token)]):
   return Controller_Copia_Agendamento.calcularProximosAgendamentos()
 
 @relatorioAPI.get("/quantidade-agendamentos", tags=["relatorios"])
-async def buscar_quantidade_agendamentos_no_mes():
+async def buscar_quantidade_agendamentos_no_mes(Authorization: Annotated[Header, Depends(validar_token)]):
   controller = Controller_Copia_Agendamento()
   return controller.calcularQuantidadeAgendamentosnoMes()
 
 @relatorioAPI.get("/valorBruto-agendamentos", tags=["relatorios"])
-async def buscar_valor_bruto_agendamentos_no_mes():
+async def buscar_valor_bruto_agendamentos_no_mes(Authorization: Annotated[Header, Depends(validar_token)]):
   controller = Controller_Copia_Agendamento()
   return controller.valorAgendamentosNoMes()
 
 @relatorioAPI.get("/media-agendamentos", tags=["relatorios"])
-async def buscar_media_agendamentos_no_mes():
+async def buscar_media_agendamentos_no_mes(Authorization: Annotated[Header, Depends(validar_token)]):
   controller = Controller_Copia_Agendamento()
   return controller.media_valor_agendamentos_no_mes()
 
