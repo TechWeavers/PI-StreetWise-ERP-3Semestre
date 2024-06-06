5c5e071ae77b17d35c21afb7036207e5b913fd1e
joao <jplima.dev@outlook.com>
2024-06-06 08:52:07 -0300

    email gambiarra

--- routes/comunicacaoRoute.py
+++ routes/comunicacaoRoute.py
@@ -92,19 +92,23 @@ async def enviar_email_retorno():
         data_atual = datetime.now()
 
         # Subtrair um dia da data atual para obter a data de ontem
-        data_retorno = data_atual + timedelta(days=15)
+        data_retorno = data_atual - timedelta(days=15)
 
         # Formatar a data de ontem no mesmo formato do objeto JSON
         data_retorno = data_retorno.strftime('%Y-%m-%dT%H:%M:%S')
         data_retorno = data_retorno[:10]
         data_retorno_formatada = str(data_retorno_formatada)
+        print(data_retorno_formatada)
 
         for event in agendamentos:
             data_evento = str(event["start"]["dateTime"]).split("T")[0]
+            print(data_evento)
 
             if data_evento == data_retorno_formatada:
               email_cliente = event["attendees"][0]["email"]
               await emailRetorno(email_cliente)
+            else:
+                return status.HTTP_200_OK
            
     except HTTPException as http_exception:
         raise http_exception
@@ -112,4 +116,3 @@ async def enviar_email_retorno():
         raise HTTPException(500, f"Erro ao enviar o e-mail: {str(e)}")
 
 app.include_router(comunicacaoAPI)
-
